aeb6b04731f41b3440cb55ffd88c4db6e18787e7
pjsha <pjsha@DESKTOP-LE3VMA5>
2018-02-13 07:32:23 -0600

    Return

---
+++ print_java/src/print_java/Return.java
@@ -0,0 +1,24 @@
+package print_java;
+
+public class Return {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int x=50;
+		int y=30;
+		
+		int answer=getsum(x,y);		
+		
+			System.out.println("Answer is" +answer);}
+		
+	
+		public static int getsum(int x,int y) {
+				
+		int result= (x+y);			
+		return result;
+		
+}
+
+}
